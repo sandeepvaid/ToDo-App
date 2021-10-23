52a38513f5e609c6645d35dc98c910f581241cfb
SANDEEP vaid <vaid77167@gmail.com>
2021-10-23 13:25:48 +0530

    Put some css  , js and also work on mobile device view and finialize the project

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,33 @@
+// HERE WE TRY TO GIVE DIFFERENT COLOR FOR DIFFERENT CATEGORY FOR OUR TASK
+
 var cat = document.getElementsByClassName("category-color");
 
 for(let i of cat){
     if(i.innerText=="DSA"){
-        i.style.backgroundColor = "green";
+        i.style.backgroundColor = "#008000";
+        i.style.borderRadius = "50px";
+        i.style.boxShadow =  "5px 5px 16px #005300","-5px -5px 16px #00ad00";
+
+
     }else if(i.innerText == "Coding"){
-        i.style.backgroundColor = "yellow";
+        i.style.backgroundColor = "#da5e0b";
+        i.style.borderRadius = "50px";
+        i.style.boxShadow =  "5px 5px 16px #8e3d07","-5px -5px 16px #ff7f0f";
 
     }else if(i.innerText == "Workout"){
-        i.style.backgroundColor = "grey";
+        i.style.backgroundColor = "#42229b";
+        i.style.borderRadius = "50px";
+        i.style.boxShadow =  "5px 5px 16px #2b1665","-5px -5px 16px #592ed1";
 
     }else if(i.innerText == 'College'){
-        i.style.backgroundColor = "blue";
+        i.style.backgroundColor = "#14c4db";
+        i.style.borderRadius = "50px";
+        i.style.boxShadow =  "5px 5px 16px #0d7f8e","-5px -5px 16px #1bffff";
 
     }else{
-        i.style.backgroundColor = "pink";
+        i.style.backgroundColor = "#771692";
+        i.style.borderRadius = "50px";
+        i.style.boxShadow =  "5px 5px 16px #0d7f8e","-5px -5px 16px #1bffff";
 
     }
 }
